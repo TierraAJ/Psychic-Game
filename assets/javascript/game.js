896b12c9ee39e9202d5aac48a2e37e446fac245a
Tierra Austin-Johnson <38711594+TbabyAJ@users.noreply.github.com>
2018-08-14 07:53:43 -0400

    trying to get Computer guess to display in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,67 @@
-$(document).ready(function() {
+    $(document).ready(function() {
 
-    // Initial Variables
-    var computerPick = Math.floor(Math.random() * 4) + 1;
-    var lockGame = false;
-    var userText = document.getElementById("user-text");
+      // Initial Variables
+      var computerPick = document.getElementById("computer-pick")
+      var userText = document.getElementById("user-text");
+      var allText = document.getElementById("all-text");
+     
+      var wins = 0;
+      var losses = 0;
+      var guessesLeft = 10;
+      
+      
+      var winsText = document.getElementById("wins-text");
+      var lossesText = document.getElementById("losses-text");
+      var guessesLeftText = document.getElementById("guesses-left-text");
+      var allowedLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    // We log the computer's pick to console to make it easier to troubleshoot
-    console.log("Computer Pick: " + computerPick);
+      var lockGame = false;
 
-    // Next, we give JavaScript a function to execute when onkeyup event fires.
-    document.onkeyup = function(event) {
-    userText.textContent = event.key;
-    };
-// Here we create the keyup event that gets the user"s pick
-    $(".btn-choice").on("click", function() {
-
-      // Here this lockGame line prevents the user from changing the option after the game is done.
-      if (lockGame !== true) {
-
-        // We get the value associated with the button the user picked from here
-        var yourPick = $(this).val();
-        console.log("Your Pick: " + yourPick);
-
-        // We then reveal the computer's pick in the html
-        $("#computer-pick").text(computerPick);
-
-        // If your pick matched the computer's pick you let them know.
-        if (parseInt(yourPick) === computerPick) {
-          $("#result").text("Yep! You got it! Refresh the page to play again.");
-          lockGame = true;
-        }
-
-        // If the numbers did not match. You also let them know
-        else {
-          $("#result").text("Nope. Refresh the page to play again.");
-          lockGame = true;
-        }
+      function computerChoice() {
+        var random = Math.floor(Math.random() * allowedLetters.length);
+        return allowedLetters[random];
       }
+      
+      var computerChose = computerChoice();
+      console.log(computerChose);
 
+      // Function to execute when onkeyup event fires to get user's pick.
+      document.onkeyup = function(event) {
+        userText.textContent = event.key;
+        allText.textContent = event.key;
+        computerChoseText.textContent = "The computer chose: " + computerChoice;
+      }; 
+
+      // Here we create the on click event that gets the user"s pick
+      $(".btn-choice").on("click", function() {
+
+        // Here this lockGame line prevents the user from changing the option after the game is done.
+        if (lockGame !== true) {
+
+          // We get the value associated with the button the user picked from here
+          var yourPick = $(this).val();
+          console.log("Your Pick: " + yourPick);
+
+          // We then reveal the computer's pick in the html
+          $("#computer-pick").text(computerPick);
+
+          // If your pick matched the computer's pick you let them know.
+          if (parseInt(yourPick) === computerPick) {
+            $("#result").text("Yep! You got it! Refresh the page to play again.");
+            lockGame = true;
+          }
+
+          // If the numbers did not match. You also let them know
+          else {
+            $("#result").text("Nope. Refresh the page to play again.");
+            lockGame = true;
+          }
+        }
+        // Display the user and computer guesses, and wins/losses/ties.
+        userText.textContent = "You chose: " + userText;
+        computerPickText.textContent = "The computer chose: " + computerPick;
+        winsText.textContent = "wins: " + wins;
+        lossesText.textContent = "losses: " + losses;
+        guessesLeftText.textContent = "ties: " + guessesLeft;
+      });
     });
-  });
-
-// Let's start by grabbing a reference to the <span> below.
-
-
